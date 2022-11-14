112948e0bcf73940ef4be84604111d7083980e05
MinHee Kim <minhee.kim@ibm.com>
2022-11-14 22:39:59 +0900

    first commit

---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from .routers import router
+import logging.config
+
+logger = logging.getLogger("productService")
+
+# Set logger name to project
+
+logger.info("START Application")
+
+# Tags for representative endpoints
+tags = [
+    {
+        "name": "routers",
+        "description": "Operations with xxx",
+    }
+]
+
+# Define Fast api and description
+app = FastAPI(
+    title="Product service for Instana",
+    description="This is a template of python MSA used in Garage project.",
+    version="0.0.1",
+    openapi_tags=tags,
+)
+
+# Add routers to main
+app.include_router(router.router)
+
+# This path is for health check or test
+@app.get("/")
+async def root():
+    return {"Hello World :D"}
---
+++ app/routers/router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+"""
+This Module is for query endpoints.
+Client can access this application through GET/POST/PUT/DELETE /queries
+"""
+
+router = APIRouter()
+
+
+@router.get("/routers", tags=["router"], description="Test endpoint")
+def test_endpoint():
+    return {"Test endpoint"}
