9fba30f30a7a671fd9425da4e34fbcd1322ff9f7
MinHee Kim <minhee.kim@ibm.com>
2022-11-15 15:58:12 +0900

    error fix

--- app/models/product.py
+++ app/models/product.py
@@ -7,8 +7,8 @@ class Product(Base):
     __tablename__ = "products"
 
     product_id = Column(Integer, primary_key=True, index=True)
-    product_name = Column(String)
-    product_price = Column(String)
-    product_quantity= Column(Integer)
+    product_name = Column(String(300))
+    product_price = Column(String(300))
+    product_quantity= Column(String(300))
 
   
