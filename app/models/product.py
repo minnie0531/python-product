ab2a2c0dbe1c9b05c7aaecd50a2df02aa012538e
MinHee Kim <minhee.kim@ibm.com>
2022-11-17 12:29:51 +0900

    check api

--- app/models/product.py
+++ app/models/product.py
@@ -9,6 +9,6 @@ class Product(Base):
     product_id = Column(Integer, primary_key=True, index=True)
     product_name = Column(String(300))
     product_price = Column(String(300))
-    product_quantity= Column(String(300))
+    product_quantity= Column(Integer)
 
   
