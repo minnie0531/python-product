d038ec734f5f321355e49e3bab9b751d91f4483e
MinHee Kim <minhee.kim@ibm.com>
2022-11-22 13:18:15 +0900

    change env to environment variables

--- app/repository/sql_session.py
+++ app/repository/sql_session.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
+username = os.environ['MYSQL_USER']
+password = os.environ['MYSQL_PW']
 
-engine = create_engine('mysql+pymysql://product:passw0rd@product-mysql/product')
+engine = create_engine("mysql+pymysql://${username}:${password}@product-mysql/product")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
